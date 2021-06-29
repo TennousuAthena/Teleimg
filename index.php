bdcf29da54bfb662f8ddfc4d0116da72949598e0
天王州アテネ <qingcaomc@gmail.com>
2021-06-29 20:09:52 +0800

    Image Preview

--- index.php
+++ index.php
@@ -1,29 +1,47 @@
 <?php
 use CustomCurl\Client;
 use Bramus\Router;
-header("content-type:application/json; charset:utf-8");
 header("x-powered-by: Teleimg v0.1beta");
 
 require __DIR__ . '/vendor/autoload.php';
 
 $router = new Router\Router();
 
-$router->get('/', function() {
+$router->post('upload', function() {
+    header("content-type:application/json; charset:utf-8");
     $curlObj = Client::init('https://telegra.ph/upload', 'post')
-        ->set('postFields', ['file' => new CURLFile('./test.jpg')])
+        ->set('postFields', ['file' => new CURLFile($_FILES["file"]["tmp_name"])])
         ->set('postFieldsBuildQuery', false)
         ->exec();
-
 
     if (!$curlObj->getStatus()) {
         exit(json_encode($curlObj->getCurlErrNo()));
     }
     $body = json_decode($curlObj->getBody());
-    if ($body == "") {
+    if ($curlObj->getInfo()['http_code'] !== 200||$body == "" || !is_array($body)) {
         halt("Failed to upload");
     }
     echo json_encode(['success'=> 1, "src"=>$body[0]->src]);
 });
+
+$router->get('/file/(.*)', function($name) {
+
+    $curlObj = Client::init('https://telegra.ph/file/'.$name)->exec();
+
+    if (!$curlObj->getStatus()) {
+        throw new \Exception('Curl Error', $curlObj->getCurlErrNo());
+    }
+    $start = strpos($curlObj->getHeader(), "content-type");
+    $end = strpos($curlObj->getHeader(), "content-length");
+    if($curlObj->getInfo()['http_code'] === 200) {
+        header("content-type: ".substr($curlObj->getHeader(), $start+14, $end-$start-16));
+        echo $curlObj->getBody();
+    }else{
+        header("content-type: image/jpeg");
+        echo file_get_contents(__DIR__ . "/akkarin.jpg");
+    }
+});
+
 // Run it!
 $router->run();
 
